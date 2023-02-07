7ccd028e75afb4b38ac1e661b71742ef1b532268
Tamani Phiri <tamanigabriel0@gmail.com>
2023-02-06 21:30:52 -0800

    left added menu

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,10 +1,22 @@
-import { View, Text, Image, TouchableOpacity,SafeAreaView } from 'react-native'
+import { View, Text, Image, TouchableOpacity,SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import Header from '../Components/Header'
 
 const Home = () => {
   return (
-    <Header/>
+    <View>
+      <Header/>
+      <ScrollView >
+        <View style={{flex:1, justifyContent:'center', paddingHorizontal:12, paddingTop:20}}>
+          {/*Intro Wrapper*/}
+          <View>
+            <Text style={{fontSize:32}}>Good Morning Enock</Text>
+            <Text style={{fontSize:16, fontWeight:"300"}}>6 devices are active now</Text>
+          </View>
+
+        </View>
+      </ScrollView>
+    </View>
   )
 }
 
